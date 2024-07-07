fa2f9618eb6422d21f85a0d12331c54c351f57b4
James Harris <jameseharrisii@gmail.com>
2024-07-07 12:17:54 -0700

    1st init commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+
+// This practice with props just the beginning with building a function app
+
+
+    function App(){
+      return (
+          <div>
+              <h1>How fast do you type?</h1>
+              <textarea />
+              <h4>Time remaining: ??? </h4>
+              <button>Start</button>
+              <h1>Word count: ???</h1>
+          </div>
+          
+      )
+  }
+
+
+export default App
